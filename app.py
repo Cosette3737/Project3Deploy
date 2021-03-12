841327728452c41e7ff8ab9e03615c639b709f88
Cosette37 <69123930+Cosette37@users.noreply.github.com>
2021-03-11 21:25:48 -0600

    bio route

--- app.py
+++ app.py
@@ -112,7 +112,9 @@ def database_data():
 def explore():
     return render_template('explore.html')
   
-
+@app.route('/bio')
+def bio():
+    return render_template('bio.html')
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
